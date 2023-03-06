6b1efc4259c5765f041efe2c112c4f6bd3214d95
Filip Troníček <filip.tronicek@seznam.cz>
2023-03-06 21:45:47 +0100

    Fix config

---
+++ src/routes/receive/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import UrlInput from '../../lib/components/Urlnput.svelte'
+  </script>
+  
+  <main class="container">
+	<h1>Interclip</h1>
+	<p>
+	  Input a code.
+	</p>
+  
+	<div class="row">
+	  <UrlInput />
+	</div>
+  </main>
+
+
+<style>
+
+</style>
