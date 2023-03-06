9ae898152e0a9d8f13e2695b428c29c6a3460a06
Filip Troníček <filip.tronicek@seznam.cz>
2023-03-06 21:23:38 +0100

    Clear unused code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,31 +15,5 @@
 
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
 
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
 </style>
