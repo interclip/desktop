799915db3687a14d3a95b74565043f9e3389e60f
Filip Troníček <filip.tronicek@seznam.cz>
2023-03-18 17:20:30 +0100

    Settings icon in menu

--- src/lib/components/MenuItem.svelte
+++ src/lib/components/MenuItem.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	export let url: string;
+	export let title: string = "";
 </script>
 
 <a href={url}>
-	<li class="menu-item px-4 py-4 cursor-pointer bg-white rounded-xl dark:bg-gray-600 dark:text-white">
+	<li class="menu-item px-4 py-4 cursor-pointer bg-white rounded-xl dark:bg-gray-600 dark:text-white" title={title}>
 		<slot />
 	</li>
 </a>
