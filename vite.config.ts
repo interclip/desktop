a049e69a144f0163c9921aec519837fc7d2fcf7f
Filip Troníček <filip.tronicek@seznam.cz>
2023-03-18 17:30:27 +0100

    Simplify icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import svelteSVG from "vite-plugin-svelte-svg";
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(), svelteSVG({
+			svgoConfig: {},
+			requireSuffix: true, 
+		}),
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
