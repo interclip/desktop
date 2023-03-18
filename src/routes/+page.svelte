150ea7d18c43a2fae06d1f912563bb736180de89
Filip Troníček <filip.tronicek@seznam.cz>
2023-03-18 11:44:35 +0100

    Format

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,17 @@
 <script lang="ts">
-	import UrlInput from '../lib/components/Urlnput.svelte'
-  </script>
-  
-  <main class="container">
+	import UrlInput from '../lib/components/Urlnput.svelte';
+</script>
+
+<main class="container">
 	<h1 class="text-3xl mb-4">Interclip</h1>
 	<p class="mb-4">
 		Input a URL <s>or drag n' drop an image</s>.
 	</p>
-  
-	<div class="row">
-	  <UrlInput />
-	</div>
-  </main>
 
+	<div class="row">
+		<UrlInput />
+	</div>
+</main>
 
 <style>
-
 </style>
--- src/routes/receive/+page.svelte
+++ src/routes/receive/+page.svelte
@@ -1,18 +1,15 @@
 <script lang="ts">
-	import CodeInput from '../../lib/components/CodeInput.svelte'
-  </script>
-  
-  <main class="container">
+	import CodeInput from '../../lib/components/CodeInput.svelte';
+</script>
+
+<main class="container">
 	<h1 class="text-3xl mb-4">Interclip</h1>
-	<p class="mb-4">
-	  Input a code.
-	</p>
-  
+	<p class="mb-4">Input a code.</p>
+
 	<div class="row">
-	  <CodeInput />
+		<CodeInput />
 	</div>
-  </main>
+</main>
 
 <style>
-
 </style>
