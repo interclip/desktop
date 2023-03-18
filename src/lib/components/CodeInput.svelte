2ace4126495b8cb114aded95194365b18969babf
Filip Troníček <filip.tronicek@seznam.cz>
2023-03-18 12:01:12 +0100

    Pass endpoint option from frontend

--- src/lib/components/CodeInput.svelte
+++ src/lib/components/CodeInput.svelte
@@ -1,28 +1,29 @@
 <script lang="ts">
-    import { invoke } from "@tauri-apps/api/tauri";
-  
-    let inputCode = "";
-    let output = "";
-  
-    async function greet() {
-      output = await invoke("retrieve_clip_cmd", { code: inputCode });
-    }
-  </script>
-  
-  <div>
-    <div class="row pb-8">
-      <form action="#" on:submit={greet}>
-        <input
-          type="text"
-          placeholder=""
-          bind:value={inputCode}
-          maxlength="5"
-          minlength="5"
-          required
-        />
-        <button type="submit">Receive</button>
-      </form>
-    </div>
-    <p>{output}</p>
-  </div>
-  
+	import { invoke } from '@tauri-apps/api/tauri';
+	import * as settings from '$lib/utils/settings';
+
+	let inputCode = '';
+	let output = '';
+
+	async function greet() {
+		const endpoint = await settings.get<string>('endpoint');
+		output = await invoke('retrieve_clip_cmd', { code: inputCode, options: { endpoint } });
+	}
+</script>
+
+<div>
+	<div class="row pb-8">
+		<form action="#" on:submit={greet}>
+			<input
+				type="text"
+				placeholder=""
+				bind:value={inputCode}
+				maxlength="5"
+				minlength="5"
+				required
+			/>
+			<button type="submit">Receive</button>
+		</form>
+	</div>
+	<p>{output}</p>
+</div>
--- src/lib/components/Urlnput.svelte
+++ src/lib/components/Urlnput.svelte
@@ -1,26 +1,22 @@
 <script lang="ts">
-    import { invoke } from "@tauri-apps/api/tauri";
-  
-    let inputUrl = "";
-    let output = "";
-  
-    async function greet() {
-      output = await invoke("create_clip_cmd", { url: inputUrl });
-    }
-  </script>
-  
-  <div>
-    <div class="row pb-8">
-      <form action="#" on:submit={greet}>
-        <input
-          type="url"
-          placeholder="https://..."
-          bind:value={inputUrl}
-          required
-        />
-        <button type="submit">Create</button>
-      </form>
-    </div>
-    <p>{output}</p>
-  </div>
-  
+	import { invoke } from '@tauri-apps/api/tauri';
+	import * as settings from '$lib/utils/settings';
+
+	let inputUrl = '';
+	let output = '';
+
+	async function greet() {
+		const endpoint = await settings.get<string>('endpoint');
+		output = await invoke('create_clip_cmd', { url: inputUrl, options: { endpoint } });
+	}
+</script>
+
+<div>
+	<div class="row pb-8">
+		<form action="#" on:submit={greet}>
+			<input type="url" placeholder="https://..." bind:value={inputUrl} required />
+			<button type="submit">Create</button>
+		</form>
+	</div>
+	<p>{output}</p>
+</div>
