97a65ffd296f5709ebdd2079e0d83349bbcbf3fa
Filip Troníček <filip.tronicek@seznam.cz>
2023-03-18 14:09:13 +0100

    Try clipboard pasting

--- src/lib/utils/clipboard.ts
+++ src/lib/utils/clipboard.ts
@@ -4,6 +4,10 @@ export const copyToClipboard = async (text: string) => {
     navigator.clipboard.writeText(text);
 }
 
+export const readFromClipboard = async () => {
+    return await navigator.clipboard.readText();
+}
+
 export const copyIfEnabled = async (text: string) => {
     if (await settings.get<boolean>('copyToClipboard')) {
         await copyToClipboard(text);
