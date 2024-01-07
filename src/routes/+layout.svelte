65f4ff9592bbd9ee65886512c806a947abd91edd
Filip Troníček <filip.tronicek@seznam.cz>
2024-01-07 23:53:25 +0100

    Remove CSS styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,31 +3,10 @@
 	import './styles.css';
 </script>
 
-<div class="app">
+<div class="app flex flex-col min-h-screen">
 	<Header />
 
-	<main>
+	<main class="flex flex-col w-full max-w-5xl m-0 box-border justify-center items-center h-screen">
 		<slot />
 	</main>
 </div>
-
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-		justify-content: center;
-		align-items: center;
-	}
-</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,16 +2,11 @@
 	import UrlInput from '../lib/components/Urlnput.svelte';
 </script>
 
-<main class="container">
-	<h1 class="text-3xl mb-4">Interclip</h1>
-	<p class="mb-4">
-		Input a URL <s>or drag n' drop an image</s>.
-	</p>
+<h1 class="text-3xl mb-4">Interclip</h1>
+<p class="mb-4">
+	Input a URL <s>or drag n' drop an image</s>.
+</p>
 
-	<div class="row">
-		<UrlInput />
-	</div>
-</main>
-
-<style>
-</style>
+<div class="row">
+	<UrlInput />
+</div>
